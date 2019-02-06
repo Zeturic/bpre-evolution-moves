f52003c60850dcd05ef0a29894934d431682a99a
Kevin Mills <kevin.mills226@gmail.com>
2019-02-05 20:47:31 -0500

    Squashed 'scripts/makinoa/' content from commit fc0f89a
    git-subtree-dir: scripts/makinoa
    git-subtree-split: fc0f89a4ae0721b5645ddff66176396fefbda7fe

---
+++ scripts/makinoa/orderedset.py
@@ -0,0 +1,19 @@
+import collections
+
+class OrderedSet():
+    def __init__(self, things=()):
+        self._dict = collections.OrderedDict()
+        for thing in things:
+            self._dict[thing] = None
+
+    def __iter__(self):
+        return iter(self._dict)
+
+    def add(self, thing):
+        self._dict[thing] = None
+
+    def remove(self, thing):
+        self._dict.pop(thing)
+
+    def __bool__(self):
+        return bool(self._dict)
