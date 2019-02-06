32a32583774637d280e8de799bc5202b2902e638
Kevin Mills <kevin.mills226@gmail.com>
2019-02-06 11:55:17 -0500

    make terminator condition explicit

--- src/global.h
+++ src/global.h
@@ -8,7 +8,7 @@
 #include "functions.h"
 
 #ifndef USE_JAMBO_LEARNSET_HACK
-#define IS_LEVEL_UP_LEARNSET_TERMINATOR(ls) (*((u16*) (ls)) == 0xFFFF)
+#define IS_LEVEL_UP_LEARNSET_TERMINATOR(ls) ((ls)->lvl == 0x7F && (ls)->move == 0x1FF)
 #else
-#define IS_LEVEL_UP_LEARNSET_TERMINATOR(ls) ((ls)->lvl == 0xFF)
+#define IS_LEVEL_UP_LEARNSET_TERMINATOR(ls) ((ls)->lvl == 0xFF && (ls)->move == 0x0)
 #endif
